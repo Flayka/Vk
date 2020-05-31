4e81e94d4a9c0de67c62aef584b5b32d8fdd38c1
Alexandra <rudakalexx@gmail.com>
2020-05-31 21:15:37 +0300

    Add class PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.VkMainPage;
+
+public class PostManager {
+    private VkMainPage[] vkMainPages;
+
+    public VkMainPage[] search (int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+// TODO: add logic
+        return null;
+    }
+
+    public void delete (int ownerId, int postId) {
+    }
+}
